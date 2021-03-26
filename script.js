f401f8cc8ced0b6eb7135b529140af76bce21d60
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-03-26 22:25:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ const request = new XMLHttpRequest(); //old school way
 request.open('GET', 'https://restcountries.eu/rest/v2/name/india');
 
 request.send();
-console.log(request.responseText);
 
 request.addEventListener('load', function () {
   console.log(this.responseText);
+
+  const data = JSON.parse(this.responseText);
+  console.log(data);
 });
