d0191192ef2e9045abaa58e86bcc841bc426e40b
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-03-26 22:28:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,4 +14,17 @@ request.send();
 request.addEventListener('load', function () {
   const [data] = JSON.parse(this.responseText);
   console.log(data);
+
+  const html = `
+  <article class="country">
+          <img class="country__img" src="" />
+          <div class="country__data">
+            <h3 class="country__name">COUNTRY</h3>
+            <h4 class="country__region">REGION</h4>
+            <p class="country__row"><span>👫</span>POP people</p>
+            <p class="country__row"><span>🗣️</span>LANG</p>
+            <p class="country__row"><span>💰</span>CUR</p>
+          </div>
+        </article>
+  `;
 });
