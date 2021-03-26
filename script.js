919687b43642e8c8ab3d2987a1a342345424d626
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-03-26 22:39:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -17,14 +17,20 @@ request.addEventListener('load', function () {
 
   const html = `
   <article class="country">
-          <img class="country__img" src="" />
+          <img class="country__img" src="${data.flag}" />
           <div class="country__data">
-            <h3 class="country__name">COUNTRY</h3>
-            <h4 class="country__region">REGION</h4>
-            <p class="country__row"><span>👫</span>POP people</p>
-            <p class="country__row"><span>🗣️</span>LANG</p>
-            <p class="country__row"><span>💰</span>CUR</p>
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
           </div>
         </article>
   `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 });
