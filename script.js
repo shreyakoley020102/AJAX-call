6246b5a0baac1434537f7ee07f0d8bbbc26d3150
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-03-26 22:20:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest(); //old school way
+
+request.open('GET', 'https://restcountries.eu/rest/v2/name/india');
+
+request.send();
