94bbf995339b8d934c22c75190efa17ba730273e
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-03-28 11:01:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -111,6 +111,7 @@ const getCountryData = function (country) {
       const neighbour = data[0].borders[0];
 
       if (!neighbour) return;
+
       //Country 2
       return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
     })
