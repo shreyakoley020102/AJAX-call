48a75528fd645180d4bb7ee97ab9bdcc51c51933
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-03-29 10:54:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,32 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, classNAme = '') {
+  const html = `
+  <article class="country ${classNAme}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 // const getCountryData = function (country) {
 //   const request = new XMLHttpRequest(); //old school way
@@ -38,27 +64,6 @@ const countriesContainer = document.querySelector('.countries');
 
 // getCountryData('russia');
 // getCountryData('usa');
-
-const renderCountry = function (data, classNAme = '') {
-  const html = `
-  <article class="country ${classNAme}">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies[0].name
-            }</p>
-          </div>
-        </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
 
 // const getCountryAndNeighbour = function (country) {
 //   const request = new XMLHttpRequest(); //old school way
@@ -118,7 +123,8 @@ const getCountryData = function (country) {
     .then(response => response.json())
     .then(data => renderCountry(data, 'neighbour'))
     .catch(err => {
-      console.error(`${err} ☹️ ☹️ `);
+      console.error(`${err} ☹️☹️`);
+      renderError(`Something went wrong ☹️☹️ ${err.message}. Try again!`);
     });
 };
 
